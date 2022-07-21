17cc1eb8457035a5497fba56103b14e538d557be
Ashstolemybike <75098246+Ashstolemybike@users.noreply.github.com>
2022-07-21 14:46:03 +0100

    prints out pokemon ability

---
+++ single_pokemon.py
@@ -0,0 +1,12 @@
+from single_pokemon_model import SinglePokemonModel
+import requests
+
+class SinglePokemon:
+
+    def __init__(self, s_pokemon):
+        self.url = "https://pokeapi.co/api/v2/pokemon/" + s_pokemon
+        self.request = requests.get(self.url)
+        self.resp_json = self.request.json()
+
+    def response_data(self):
+        return SinglePokemonModel(self.resp_json)
---
+++ trying.py
@@ -0,0 +1,6 @@
+from single_pokemon import SinglePokemon
+
+ditto = SinglePokemon("ditto")
+
+print(ditto)
+print(ditto.response_data())
